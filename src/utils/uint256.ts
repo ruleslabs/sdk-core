4197cf85d2d030f7e6a8847d10a210bff2024902
xshitaka <clanier.dev@gmail.com>
2022-04-30 13:41:39 +0200

    Added uint256 helper

--- src/utils/uint256.ts
+++ src/utils/uint256.ts
@@ -7,6 +7,10 @@ function asciiToHex(str: string): string {
   return str.split('').map((c) => c[0].charCodeAt(0).toString(16)).join('')
 }
 
+export function uint256HexToStrHex(uint256: Uint256) {
+  return `${uint256.high}${uint256.low.substring(2)}`
+}
+
 // convert the first 32 characters of a string to starknet uint256
 export function strToUint256(str: string): Uint256 {
   const low = asciiToHex(str.substring(0, 16))
