61c3aa8fb2cb9514f0656b6939be3f79c5385c39
xshitaka <clanier.dev@gmail.com>
2022-07-12 15:05:44 +0200

    Updated cardId computation

--- src/utils/cardId.ts
+++ src/utils/cardId.ts
@@ -1,35 +1,15 @@
-import { keccak256 } from '@ethersproject/keccak256'
-
 function asciiToHex(str: string): string {
 	return str.split('').map(c => Number(c.charCodeAt(0)).toString(16)).join('')
 }
 
-function switchHexEndianness(hex: string): string {
-  const arr = []
-
-  for (var i = 0, l = hex.length; i < l; i += 2) arr.push(hex.substring(i, i + 2))
-  return arr.reverse().join('')
-}
-
-function switchHexEndiannessByWords(hex: string, wordLen: number): string {
-  const arr = []
-
-  for (var i = 0, l = hex.length; i < l; i += wordLen) arr.push(switchHexEndianness(hex.substring(i, i + wordLen)))
-  return arr.join('')
-}
-
 export function getStarknetCardId(artistName: string, season: number, scarcity: number, serialNumber: number): string {
-  const littleEndianHexArtistName = switchHexEndianness(asciiToHex(artistName))
-  const words: string[] = []
+  const hexArtistName = asciiToHex(artistName)
+  const low = hexArtistName.substring(0, 32).padStart(32, '0')
+  const high = hexArtistName.substring(32, 54).padStart(22, '0')
 
-  for (var i = 3 * 16; i >= 0; i -= 16) words.push(littleEndianHexArtistName.substring(i, i + 16).padEnd(16, '0'))
+	const hexSeason = season.toString(16).padStart(2, '0')
+	const hexScarcity = scarcity.toString(16).padStart(2, '0')
+	const hexSerialNumber = serialNumber.toString(16).padStart(6, '0')
 
-  words.push(switchHexEndianness(
-    (((season & 0xffff) * 2 ** 40) + ((scarcity & 0xff) * 2 ** 32) + (serialNumber & 0xffffffff)).toString(16)
-    .padStart(14, '0')
-  ))
-
-  const keccackOutput = keccak256(`0x${words.join('')}`)
-
-  return `0x${switchHexEndiannessByWords(keccackOutput.substring(2), 16)}`
+  return `0x${hexSerialNumber}${hexSeason}${hexScarcity}${high}${low}`
 }
