f7bcf8cb2d2d7938159ef207eccb2df815d98eb7
xshitaka <clanier.dev@gmail.com>
2023-06-08 00:05:08 +0200

    add `getCardModelId`

--- src/utils/tokens.ts
+++ src/utils/tokens.ts
@@ -1,17 +1,26 @@
-import { hash, shortString } from 'starknet'
+import { hash, shortString, uint256 } from 'starknet'
 
 import { Uint256 } from '../types'
 
-interface Card {
+interface CardModel {
   artistName: string
   season: number
   scarcity: number
+}
+
+interface Card extends CardModel {
   serialNumber: number
 }
 
 export function getCardTokenId({ artistName, season, scarcity, serialNumber }: Card): Uint256 {
   return {
-    low: hash.computeHashOnElements([shortString.encodeShortString(artistName), season, scarcity]),
+    low: getCardModelId({ artistName, season, scarcity }).toString(),
     high: `${serialNumber}`,
   }
+}
+
+export function getCardModelId({ artistName, season, scarcity }: CardModel) {
+  const fullId = hash.computeHashOnElements([shortString.encodeShortString(artistName), season, scarcity])
+
+  return uint256.bnToUint256(fullId).low
 }
